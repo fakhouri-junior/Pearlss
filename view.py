8c35bd22cb52d5710e627a69e4f4d8c5426a61bf
salim <ceaserfs3@gmail.com>
2016-09-19 22:20:11 +0800

    reqs messy

--- view.py
+++ view.py
@@ -1,12 +1,4 @@
 from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
-from flask import session as login_session
-import random, string
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
-import httplib2
-import json
-from flask import make_response
-import requests
 
 from flask_mail import Mail, Message
 
