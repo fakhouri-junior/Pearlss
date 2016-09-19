f8c291cd2845bdf197eeeb64b7948990c114c138
salim <ceaserfs3@gmail.com>
2016-09-19 22:16:44 +0800

    reqs messy

--- view.py
+++ view.py
@@ -71,7 +71,7 @@ def sendEmail():
     else:
         return "REJECTED"
 
-if __name__ == '__main__':
-  app.secret_key = 'super_secret_key'
-  app.debug = True
-  app.run(host = '0.0.0.0', port = 5000)
+#if __name__ == '__main__':
+app.secret_key = 'super_secret_key'
+app.debug = False
+#app.run(host = '0.0.0.0', port = 5000)
