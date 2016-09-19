9346c18a528f76a5968413174992fb42dcafde9e
salim <ceaserfs3@gmail.com>
2016-09-19 13:07:04 +0800

    update New

--- view.py
+++ view.py
@@ -8,7 +8,13 @@ import json
 from flask import make_response
 import requests
 
+from flask_mail import Mail, Message
+
 app = Flask(__name__)
+
+
+mail = Mail(app)
+
 
 
 @app.route('/')
@@ -20,6 +26,31 @@ def homePage():
 @app.route('/test')
 def showTest():
     return render_template('test.html')
+
+
+@app.route('/email', methods=['POST'])
+def sendEmail():
+    if request.method == 'POST':
+        if request.form['name']:
+            name = request.form['name']
+        if request.form['email']:
+            email = request.form['email']
+        if request.form['subject']:
+            subject = request.form['subject']
+        if request.form['message']:
+            message = request.form['message']
+        '''
+
+        msg = Message(sender=email,
+                      recipients=['ceaserfs3@gmail.com'],
+                      body=message,
+                      subject=subject)
+        mail.send(msg)
+        '''
+        print "got it"
+        return redirect(url_for('homePage'))
+    else:
+        return "REJECTED"
 
 if __name__ == '__main__':
   app.secret_key = 'super_secret_key'
